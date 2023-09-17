0f4aeb83edd9b2b75c6abca81ce68480eda8aa5a
Kirito <LoickEmadesemadisson@gmail.com>
2023-09-17 23:31:04 +0300

    navbar

--- frontend/src/pages/Admin_panel.tsx
+++ frontend/src/pages/Admin_panel.tsx
@@ -7,6 +7,8 @@ import { faCalendar } from "@fortawesome/free-solid-svg-icons";
 import { faComment } from "@fortawesome/free-solid-svg-icons";
 import { faGauge } from "@fortawesome/free-solid-svg-icons";
 import { faUsers } from "@fortawesome/free-solid-svg-icons";
+import { faPlus } from "@fortawesome/free-solid-svg-icons";
+import { faBox } from "@fortawesome/free-solid-svg-icons";
 import admin from "./../Assets/pdp.jpg";
 
 const Admin_panel = () => {
@@ -62,13 +64,13 @@ const Admin_panel = () => {
                </div>
 
                <div className="sidebar_content text-start">
-                   <ul>
-                      <li><FontAwesomeIcon icon={faGauge} className="icon" /> <h1> Dashboard</h1></li>
+                   <ul className="te">
+                      <li className="active"><FontAwesomeIcon icon={faGauge} className="icon" /> <h1> Dashboard</h1></li>
                       <li><FontAwesomeIcon icon={faUsers} className="icon" /> <h1>Liste employés</h1></li>
-                      <li><FontAwesomeIcon icon={faGauge} className="icon" /> <h1>Ajouter des employés</h1></li>
-                      <li><FontAwesomeIcon icon={faGauge} className="icon" /> <h1>Gestion de Stock</h1></li>
-                      <li><FontAwesomeIcon icon={faGauge} className="icon" /> <h1>Annonces</h1></li>
-                      <li><FontAwesomeIcon icon={faGauge} className="icon" /> <h1>Emplois du temps</h1></li>
+                      <li><FontAwesomeIcon icon={faPlus} className="icon" /> <h1>Ajouter des employés</h1></li>
+                      <li><FontAwesomeIcon icon={faBox} className="icon" /> <h1>Gestion de Stocks</h1></li>
+                      <li><FontAwesomeIcon icon={faBullhorn} className="icon" /> <h1>Annonces</h1></li>
+                      <li><FontAwesomeIcon icon={faCalendar} className="icon" /> <h1>Emplois du temps</h1></li>
                       <li></li>
                    </ul>
 
