7e5398f21858a2de95143bd6ed4e269e748a6a65
Kirito <LoickEmadesemadisson@gmail.com>
2023-09-18 07:15:06 +0300

    dashboard en cours

--- frontend/src/pages/Admin_panel.tsx
+++ frontend/src/pages/Admin_panel.tsx
@@ -9,6 +9,7 @@ import { faGauge } from "@fortawesome/free-solid-svg-icons";
 import { faUsers } from "@fortawesome/free-solid-svg-icons";
 import { faPlus } from "@fortawesome/free-solid-svg-icons";
 import { faBox } from "@fortawesome/free-solid-svg-icons";
+import { faSitemap } from "@fortawesome/free-solid-svg-icons";
 import admin from "./../Assets/pdp.jpg";
 
 const Admin_panel = () => {
@@ -44,37 +45,52 @@ const Admin_panel = () => {
 
       <div className="main-container">
         <div className="sidebar-wrapper" id="sidebar">
-               <div className="profil text-center">
-                     <div className="profil_image">
-                         <img src={admin} alt="" />
-                     </div>
+          <div className="profil text-center">
+            <div className="profil_image">
+              <img src={admin} alt="" />
+            </div>
 
-                     <div className="profil_contents">
-                          <h1>PulseManage</h1>
-                          <h2>Administrateur</h2>
-                     </div>
+            <div className="profil_contents">
+              <h1>PulseManage</h1>
+              <h2>Administrateur</h2>
+            </div>
 
-                     <div className="profil_icons">
-                     <FontAwesomeIcon icon={faCalendar} className="icone" />
-                     <FontAwesomeIcon icon={faBullhorn} className="icone" />
-                     <FontAwesomeIcon icon={faComment} className="icone" />
-                     
-                     </div>
+            <div className="profil_icons">
+              <FontAwesomeIcon icon={faCalendar} className="icone" />
+              <FontAwesomeIcon icon={faBullhorn} className="icone" />
+              <FontAwesomeIcon icon={faComment} className="icone" />
+            </div>
+          </div>
 
-               </div>
-
-               <div className="sidebar_content text-start">
-                   <ul className="te">
-                      <li className="active"><FontAwesomeIcon icon={faGauge} className="icon" /> <h1> Dashboard</h1></li>
-                      <li><FontAwesomeIcon icon={faUsers} className="icon" /> <h1>Liste employés</h1></li>
-                      <li><FontAwesomeIcon icon={faPlus} className="icon" /> <h1>Ajouter des employés</h1></li>
-                      <li><FontAwesomeIcon icon={faBox} className="icon" /> <h1>Gestion de Stocks</h1></li>
-                      <li><FontAwesomeIcon icon={faBullhorn} className="icon" /> <h1>Annonces</h1></li>
-                      <li><FontAwesomeIcon icon={faCalendar} className="icon" /> <h1>Emplois du temps</h1></li>
-                      <li></li>
-                   </ul>
-
-               </div>
+          <div className="sidebar_content text-start">
+            <ul className="te">
+              <li className="active">
+                <FontAwesomeIcon icon={faGauge} className="icon" />{" "}
+                <h1> Dashboard</h1>
+              </li>
+              <li>
+                <FontAwesomeIcon icon={faUsers} className="icon" />{" "}
+                <h1>Liste employés</h1>
+              </li>
+              <li>
+                <FontAwesomeIcon icon={faPlus} className="icon" />{" "}
+                <h1>Ajouter des employés</h1>
+              </li>
+              <li>
+                <FontAwesomeIcon icon={faBox} className="icon" />{" "}
+                <h1>Gestion de Stocks</h1>
+              </li>
+              <li>
+                <FontAwesomeIcon icon={faBullhorn} className="icon" />{" "}
+                <h1>Annonces</h1>
+              </li>
+              <li>
+                <FontAwesomeIcon icon={faCalendar} className="icon" />{" "}
+                <h1>Emplois du temps</h1>
+              </li>
+              <li></li>
+            </ul>
+          </div>
         </div>
 
         <div className="app-container">
@@ -82,27 +98,51 @@ const Admin_panel = () => {
             <div className="container">
               <div className="row gx-3">
                 <div className="col-3">
-                  <div className="gag">
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                   
+                  <div className="box d-flex" id="box1">
+                    <div className="box_content">
+                      <p>Employés</p>
+                      <h3>455</h3>
+                    </div>
+
+                    <div className="box_icones">
+                      <FontAwesomeIcon icon={faUsers} className="icon" />
+                    </div>
                   </div>
                 </div>
                 <div className="col-3">
-                  <div className="gag">
-                    rem ipsum dolor sit amet consectetur adipisicing eli
-                    
+                  <div className="box d-flex" id="box2">
+                    <div className="box_content">
+                      <p>Clients</p>
+                      <h3>455</h3>
+                    </div>
+
+                    <div className="box_icones">
+                      <FontAwesomeIcon icon={faUsers} className="icon" />
+                    </div>
                   </div>
                 </div>
                 <div className="col-3">
-                  <div className="gag">
-                    rem ipsum dolor sit amet consectetur adipisicing elit.
-                 
+                <div className="box d-flex" id="box3">
+                  <div className="box_content">
+                      <p>Branches</p>
+                      <h3>13</h3>
+                    </div>
+
+                    <div className="box_icones">
+                      <FontAwesomeIcon icon={faSitemap} className="icon" />
+                    </div>
                   </div>
                 </div>
                 <div className="col-3">
-                  <div className="gag">
-                    rem ipsum dolor sit amet consectetur adipisicing elit.
-                    
+                <div className="box d-flex" id="box4">
+                  <div className="box_content">
+                      <p>Stocks</p>
+                      <h3>13</h3>
+                    </div>
+
+                    <div className="box_icones">
+                      <FontAwesomeIcon icon={faBox} className="icon" />
+                    </div>
                   </div>
                 </div>
               </div>
@@ -115,5 +155,3 @@ const Admin_panel = () => {
 };
 
 export default Admin_panel;
-
-
