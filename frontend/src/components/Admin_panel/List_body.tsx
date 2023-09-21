2189d70b783f94c06d28aa1623963ee761c0590f
Kirito <LoickEmadesemadisson@gmail.com>
2023-09-21 11:03:41 +0300

    liste loading

---
+++ frontend/src/components/Admin_panel/List_body.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const List_body = () => {
+    return (
+        <div className='list-container'>
+               
+        </div>
+    );
+};
+
+export default List_body;
--- frontend/src/pages/Admin_panel/Liste_employés.tsx
+++ frontend/src/pages/Admin_panel/Liste_employés.tsx
@@ -1,15 +1,33 @@
-import React from 'react';
-import Header from '../../components/Admin_panel/Header';
-import List_nav from '../../components/Admin_panel/List_nav';
+import React from "react";
+import Header from "../../components/Admin_panel/Header";
+import List_nav from "../../components/Admin_panel/List_nav";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faHome } from "@fortawesome/free-solid-svg-icons";
 
 const Liste_employés: React.FC = () => {
-    return (
-        <div className='liste_employés'>
-              <Header/>
-              <List_nav/>
+  return (
+    <div className="liste_employés">
+      <Header />
+
+      <div className="app-container">
+        <List_nav />
+
+        <div className="list-container">
+          <div className="navigation d-flex">
+            <div className="navigation_1 d-flex">
+              <div className="home d-flex">
+                <FontAwesomeIcon icon={faHome} className="icon" />
+                <h1>Accueil</h1>
+              </div>
+              <div className="linkto">
+                <h1>&gt; Liste employés</h1>
+              </div>
+            </div>
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default Liste_employés;
-
